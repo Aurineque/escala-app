7046b80d46ede1520f7784e0b9e11558e153cef2
Aurineque <aurinequecsf@gmail.com>
2026-04-29 07:24:44 -0300

    Refatora lógica de escalas no evento para limitar escalas no mês

--- escala/views.py
+++ escala/views.py
@@ -19,27 +19,36 @@ def voluntariar(request, evento_id):
     evento = get_object_or_404(Evento, id=evento_id)
     
     if request.method == 'POST':
-        form = EscalaForm(request.POST)
+        form = EscalaForm(request.POST) # Lembre-se de usar o nome correto do seu form
+        
         if form.is_valid():
-            # Pega qual membro a pessoa escolheu na caixinha antes de salvar
-            membro_escolhido = form.cleaned_data['membro']
+            membro = form.cleaned_data['membro']
             
-            # TRATAMENTO DE ERRO: Verifica se já existe uma escala com esse membro + esse evento
-            if Escala.objects.filter(evento=evento, membro=membro_escolhido).exists():
-                # Dispara a mensagem de erro e NÃO salva
-                messages.error(request, f"Atenção: {membro_escolhido.nome} já está escalado(a) para este evento!")
-            else:
-                # Se não existir, salva normalmente!
-                escala = form.save(commit=False)
-                escala.evento = evento
-                escala.save()
-                
-                # Dispara a mensagem de sucesso
-                messages.success(request, f"Sucesso! Presença de {membro_escolhido.nome} confirmada com alegria!")
-                
-                # Manda o usuário de volta para a tela inicial
-                return redirect('lista_eventos') 
+            # --- REGRA 1: Impede de se escalar duas vezes no MESMO evento ---
+            if Escala.objects.filter(evento=evento, membro=membro).exists():
+                messages.error(request, f"Ops! {membro.nome} já está escalado(a) neste evento.")
+                return redirect('voluntariar', evento_id=evento.id)
+            
+            # --- REGRA 2: Limite máximo escalas no mês ---
+            escalas_no_mes = Escala.objects.filter(
+                membro=membro,
+                evento__data__month=evento.data.month,
+                evento__data__year=evento.data.year
+            ).count()
+            
+            if escalas_no_mes >= 6:
+                messages.error(request, f"{membro.nome} já atingiu o limite de 3 escalas para este mês!")
+                return redirect('voluntariar', evento_id=evento.id) 
+            
+            # --- Se passou pelos dois bloqueios, salva com sucesso ---
+            escala = form.save(commit=False)
+            escala.evento = evento
+            escala.save()
+            
+            messages.success(request, "Presença confirmada com sucesso!")
+            return redirect('lista_eventos')
+            
     else:
         form = EscalaForm()
-
-    return render(request, 'escala/voluntariar.html', {'evento': evento, 'form': form})
+        
+    return render(request, 'escala/voluntariar.html', {'form': form, 'evento': evento})
